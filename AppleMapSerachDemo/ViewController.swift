801e9e37993c8e9e68252f8646768bed6eae9d88
Chetan Kailodia <chetan@idealittechno.com>
2019-12-25 15:49:58 +0530

    Add

--- AppleMapSerachDemo/ViewController.swift
+++ AppleMapSerachDemo/ViewController.swift
@@ -47,7 +47,7 @@ class ViewController: UIViewController {
         definesPresentationContext = true
         locationSearchTable.mapView = mapView
         locationSearchTable.handleMapSearchDelegate = self
-        //This is the Text Changes Map Search Hi this Is chetan on Search Branch
+        //This is the Text Changes Map Search Hi this Is chetan on Search Branch To Add
     }
     
     func getDirections(){
